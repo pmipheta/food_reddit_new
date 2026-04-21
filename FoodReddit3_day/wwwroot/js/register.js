b23a883b76923e3e5fa0a64c1a2455552a0f445e
princemiphetattamongkhon@gmail.com <princemiphetattamongkhon@gmail.com>
2026-04-22 00:02:41 +0700

    add register page with css and js separation

---
+++ FoodReddit3_day/wwwroot/js/register.js
@@ -0,0 +1,11 @@
+﻿function togglePw() {
+    const input = document.getElementById('PasswordHash');
+    const btn = document.getElementById('toggleBtn');
+    if (input.type === 'password') {
+        input.type = 'text';
+        btn.textContent = '🙈';
+    } else {
+        input.type = 'password';
+        btn.textContent = '👁';
+    }
+}
